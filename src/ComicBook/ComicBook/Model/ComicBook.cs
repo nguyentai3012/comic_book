06011cc12a00a73d5d642ef5aa19eb22f9a7ecd5
thanhndlv <thanhndlv>
2020-08-26 09:28:09 +0700

    Updated comicbook model

--- src/ComicBook/ComicBook/Model/ComicBook.cs
+++ src/ComicBook/ComicBook/Model/ComicBook.cs
@@ -7,11 +7,26 @@ namespace ComicBook.Model
 {
     public class ComicBook
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHTML { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
